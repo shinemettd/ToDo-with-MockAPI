0b9ae3beca49844c0c5877b538d4e0feada5be4d
David <dsxvqq@gmail.com>
2023-12-20 10:54:58 +0600

    added statuses to the tasks and its sorting

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -1,18 +1,23 @@
 // Retrieve the user ID from local storage
 var userId = localStorage.getItem('userId');
 
+var sortBy = 'Default';
+
 /**
  * Adds a new task to the user's tasks list.
  * @param {string} name - The name of the task.
  * @param {string} description - The description of the task.
  * @param {string} deadline - The deadline of the task.
  */
-function addTask(name, description, deadline) {
+function addTask(name, description, deadline, status) {
+    
     const newTask = {
         task_name: name,
         task_description: description,
-        task_deadline: deadline
+        task_deadline: deadline,
+        task_status: status
     };
+
 
     // Make a POST request to add the new task
     fetch('https://6566cae464fcff8d730f1095.mockapi.io/api/v1/user/'+ userId +'/task', {
@@ -51,6 +56,12 @@ function deleteTask(id) {
 // Object to handle various task-related actions
 var myObj = {
     /**
+     * Selecting task description from html file.
+     */
+    textSelect: function(){
+        document.getElementById('description').select();
+    },
+    /**
      * Hides the task form.
      */
     hide: function() {
@@ -76,8 +87,56 @@ var myObj = {
         // Enable editing for task name and description
         document.getElementById('editTaskName_' + buttonId).removeAttribute('readonly');
         document.getElementById('editTaskDescription_' + buttonId).removeAttribute('readonly');
+        document.getElementById('changeStatus_' + buttonId).style.display = 'flex';
         document.getElementById(buttonId).innerText = 'Save';
         document.getElementById(buttonId).addEventListener('click', myObj.saveChanges);
+    },
+
+    changeStatus: async function() {
+        var buttonId = this.getAttribute('id').split('_')[1]; 
+    
+        var myTasks = await returnToDo();
+        var taskToUpdate = myTasks[buttonId];
+        if (taskToUpdate.task_status == 'Failed') {
+            alert('You already failed that task!');
+            return;
+        }
+        // Make a GET request to find task we need
+        fetch('https://6566cae464fcff8d730f1095.mockapi.io/api/v1/user/' + userId + '/task', {
+            method: 'GET',
+            headers: { 'content-type': 'application/json' },
+        })
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error('Failed to fetch user data');
+            }
+        }).then(tasks => {
+            const task = tasks.find(t => t.task_name === taskToUpdate.task_name && t.task_description == taskToUpdate.task_description);
+            if (task) {
+                var updateTaskId = task.id;
+                var newStatus = (taskToUpdate.task_status === 'In progress') ? 'Completed' : 'In progress';
+                // Make a PUT request to change the task status
+                fetch('https://6566cae464fcff8d730f1095.mockapi.io/api/v1/user/' + userId + '/task/' + updateTaskId, {
+                    method: 'PUT',
+                    headers: {'content-type':'application/json'},
+                    body: JSON.stringify({task_status: newStatus})
+                }).then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+                }).then(task => {
+                    showMyTasks();
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
     },
 
     /**
@@ -110,7 +169,6 @@ var myObj = {
         }).then(tasks => {
             const task = tasks.find(t => t.task_name === taskToUpdate.task_name && t.task_description == taskToUpdate.task_description);
             if (task) {
-                console.log('task to change found');
                 var updateTaskId = task.id;
                 // Make a PUT request to change the task
                 fetch('https://6566cae464fcff8d730f1095.mockapi.io/api/v1/user/' + userId + '/task/' + updateTaskId, {
@@ -127,12 +185,32 @@ var myObj = {
                 .catch(error => {
                     console.log(error);
                 })
-
             }
         })
         .catch(error => {
             console.log(error);
         });
+    },
+
+    changeSort: async function() {
+        var sortButton = document.getElementById('sortButton');
+
+        switch (sortBy) {
+            case "Deadline":
+                sortBy = "Default";
+                break;
+            case "Default":
+                sortBy = "Task Status";
+                break;
+            case "Task Status":
+                sortBy = "Deadline";
+                break;
+            default:
+                sortBy = "Default";
+        }
+
+        sortButton.innerText = 'Sort By: ' + sortBy;
+        showMyTasks();
     },
 
     /**
@@ -186,9 +264,14 @@ var myObj = {
  */
 async function returnToDo() {
     var myTasks = [];
-
+    var url = new URL('https://6566cae464fcff8d730f1095.mockapi.io/api/v1/user/' + userId + '/task');
+    if (sortBy === 'Deadline') {
+        url.searchParams.append('sortBy', 'task_deadline');
+    } else if (sortBy === 'Task Status') {
+        url.searchParams.append('sortBy', 'task_status');
+    }
     try {
-        const res = await fetch('https://6566cae464fcff8d730f1095.mockapi.io/api/v1/user/' + userId + '/task', {
+        const res = await fetch(url, {
             method: 'GET',
             headers: { 'content-type': 'application/json' },
         });
@@ -212,14 +295,34 @@ function Task() {
     this.name = document.getElementById('taskName').value;
     this.date = document.getElementById('myDate').value;
     this.describe = document.getElementById('description').value;
+    var currentDate = new Date();
+    var targetDate = new Date(this.date);
+    targetDate.setHours(23);
+    targetDate.setMinutes(59);
+    targetDate.setSeconds(59);
+    if (currentDate < targetDate || currentDate === targetDate) {
+        this.status = 'In progress';
+    } else {
+        this.status = 'Failed';
+    }
 }
 
-function newTask(x, y, o, index) {
+function newTask(x, y, o, status, index) {
+    var otherStatus;
+    if (status === 'In progress') {
+        otherStatus = 'Completed';
+    } else if (status === 'Completed') {
+        otherStatus = 'In progress';
+    } else {
+        otherStatus === 'Failed';
+    }
     document.getElementById('myTasks').innerHTML +=
             '<div class="col l3 m4 s12 animated zoomIn" style="margin-top: 20px;">' +
             '<strong><textarea style="font-size: 30px;" id="editTaskName_' + index + '" readonly>' + x + '</textarea><strong><br>' +
             '<textarea id="editTaskDescription_' + index + '"readonly>' + y + '</textarea><br>' +
             '<h3>Due: ' + o + ' </h3><br>' +
+            '<p>Status: ' + status + '</p>' +
+            '<div class="btn blue" id="changeStatus_' + index + '" style="display: none; margin-bottom: 10px; align-items: center; justify-content: center;">Change to ' + otherStatus + '</div>' +
             '<div class="btn green" id="' + index + '">Edit</div>' +
             '<div class="btn red" id="' + index + '">Delete</div>' +
         '</div>'
@@ -236,6 +339,7 @@ async function showMyTasks(){
             myTasks[i].task_name,
             myTasks[i].task_description,
             myTasks[i].task_deadline,
+            myTasks[i].task_status,
             i
         );
     }
@@ -247,6 +351,10 @@ async function showMyTasks(){
     for (var k = 0; k < editButton.length; k++) {
         editButton[k].addEventListener('click', myObj.edit);
     }
+    var changeStatusButton = document.getElementsByClassName('blue');
+    for (var l = 0; l < changeStatusButton.length; l++) {
+        changeStatusButton[l].addEventListener('click', myObj.changeStatus);
+    }
 }
 
 /**
@@ -257,7 +365,7 @@ async function submitInfo(){
     var task = new Task();
     myTasks.push(task);
     localStorage.setItem('myData', JSON.stringify(myTasks));
-    addTask(task.name, task.describe, task.date);
+    addTask(task.name, task.describe, task.date, task.status);
     showMyTasks();
     myObj.hide();
 }
